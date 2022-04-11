db10630bdba46c7d29b24bdb73274188352f404d
Dinesh1042 <dineshram1042002@gmail.com>
2022-04-11 18:45:47 +0530

    remove fullpage scroll

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'home',
@@ -6,36 +6,31 @@ import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  @ViewChild('carousel', { read: ElementRef })
-  carouselElRef?: ElementRef<HTMLElement>;
-  @ViewChild('product', { read: ElementRef })
-  productElRef?: ElementRef<HTMLElement>;
-
-  prevValue = 0;
-  currentValue = 0;
-  isRunning = false;
-
-  @HostListener('window:scroll') onScroll() {
-    this.prevValue = this.currentValue;
-    this.currentValue = window.scrollY;
-
-    const top = (this.productElRef?.nativeElement as HTMLElement).offsetTop;
-    const isScrollingDown = this.currentValue > this.prevValue;
-    const carouselHeight = (this.carouselElRef?.nativeElement as HTMLElement)
-      .clientHeight;
-
-    if (this.currentValue > top || !isScrollingDown) return;
-
-    if (
-      isScrollingDown &&
-      this.currentValue < carouselHeight &&
-      !this.isRunning
-    ) {
-      this.isRunning = true;
-      window.scrollTo({ top: top - 36, behavior: 'smooth' });
-    }
-
-    if (!isScrollingDown || !(this.currentValue < carouselHeight))
-      this.isRunning = false;
-  }
+  // TODO: To be implemented full page scrolling
+  // @ViewChild('carousel', { read: ElementRef })
+  // carouselElRef?: ElementRef<HTMLElement>;
+  // @ViewChild('product', { read: ElementRef })
+  // productElRef?: ElementRef<HTMLElement>;
+  // prevValue = 0;
+  // currentValue = 0;
+  // isRunning = false;
+  // @HostListener('window:scroll') onScroll() {
+  // this.prevValue = this.currentValue;
+  // this.currentValue = window.scrollY;
+  // const top = (this.productElRef?.nativeElement as HTMLElement).offsetTop;
+  // const isScrollingDown = this.currentValue > this.prevValue;
+  // const carouselHeight = (this.carouselElRef?.nativeElement as HTMLElement)
+  //   .clientHeight;
+  // if (this.currentValue > top || !isScrollingDown) return;
+  // if (
+  //   isScrollingDown &&
+  //   this.currentValue < carouselHeight &&
+  //   !this.isRunning
+  // ) {
+  //   this.isRunning = true;
+  //   window.scrollTo({ top: top - 36, behavior: 'smooth' });
+  // }
+  // if (!isScrollingDown || !(this.currentValue < carouselHeight))
+  //   this.isRunning = false;
+  // }
 }
